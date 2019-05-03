5619d74814d7a75439d4cdee593929d510f384e7
dave griffiths <dave@fo.am>
2019-05-03 17:30:37 +0100

    fixed: #105

--- app/src/main/assets/crop-tree-menu.scm
+++ app/src/main/assets/crop-tree-menu.scm
@@ -145,6 +145,125 @@
         (tree-menu 'removed 'process (list 'feed 'malt))
         'wholecrop))))
 
+    'beetroot
+
+    (tree-menu
+     'brussels-sprout-cabbage 'subtype1
+     (list
+      'brussels-sprout
+      'storage-cabbage
+      'head-cabbage-pre-31-dec
+      'head-cabbage-post-31-dec
+      'collard-pre-31-dec
+      'collard-post-31-dec))
+
+    'bulb
+
+    'carrot
+
+    (tree-menu
+     'cauliflower-calabrese 'subtype1
+     (list
+      'cauliflower-summer-autumn
+      (tree-menu 
+       'cauliflower-winter-hardy-roscoff 'subtype2
+       (list 'seedbed 'top-dressing))
+      'calabrese))
+
+
+    'fodder-beet
+
+    (tree-menu
+     'grass 'subtype
+     (list
+      (tree-menu
+       'established 'sown (list 'spring 'summer-autumn 'clover))
+      (tree-menu
+       'grazed
+       'targetyield
+       (list 'DM4-5 'DM5-7 'DM6-8 'DM7-9 'DM9-12 'DM10-13 'DM12-15+))
+      'hay
+      'rye      
+      (tree-menu
+       'silage 'targetyield
+       (list
+        (tree-menu 'DM5-7 'cut (list 'one 'two 'three 'four))
+        (tree-menu 'DM7-9 'cut (list 'one 'two 'three 'four))
+        (tree-menu 'DM9-12 'cut (list 'one 'two 'three 'four))
+        (tree-menu 'DM12-15+ 'cut (list 'one 'two 'three 'four))))))
+
+    'kale-grazed
+
+    (tree-menu
+     'lettuce-leafy-salad 'subtype1
+     (list 'lettuce-whole-head 'lettuce-baby-leaf 'wild-rocket))
+    
+    'linseed
+
+    'maize
+
+    (tree-menu
+     'oat 'sown
+     (list
+      (tree-menu
+       'winter 'application (list 'forage 'incorporated 'removed))
+      (tree-menu
+       'spring 'application (list 'forage 'incorporated 'removed))))
+
+    (tree-menu
+     'rape 'use
+     (list 
+      'forage 
+      (tree-menu 
+       'oilseed 'sown
+       (list 'winter 'spring))))
+    
+    (tree-menu
+     'onion-leek 'subtype1
+     (list 'bulb-onion 'salad-onion 'leek))
+
+    'pea-bean
+
+    (tree-menu 
+     'potato 'determinancy-group
+     (list
+      (tree-menu 
+       'one 'growing-season-length
+       (list '<60 '60-90 '90-120 '>120))
+      (tree-menu 
+       'two 'growing-season-length
+       (list '<60 '60-90 '90-120 '>120))
+      (tree-menu 
+       'three 'growing-season-length
+       (list '<60 '60-90 '90-120 '>120))
+      (tree-menu 
+       'four 'growing-season-length
+       (list '<60 '60-90 '90-120 '>120))))
+    
+    (tree-menu
+     'rye 'sown
+     (list
+      (tree-menu
+       'winter 'application (list 'forage 'incorporated 'removed))
+      (tree-menu
+       'spring 'application (list 'forage 'incorporated 'removed))))
+
+    (tree-menu
+     'swede 'use
+     (list 'forage-lifted 'forage-grazed 'vegetable))
+
+    (tree-menu
+     'triticale 'sown
+     (list
+      (tree-menu
+       'winter 'application (list 'forage 'incorporated 'removed))
+      (tree-menu
+       'spring 'application (list 'forage 'incorporated 'removed))))
+
+    (tree-menu
+     'turnip 'use
+     (list 'forage-lifted 'forage-grazed 'vegetable))
+
     (tree-menu
      'wheat 'sown
      (list
@@ -161,116 +280,6 @@
         (tree-menu 'removed 'process (list 'feed 'mill))
         'wholecrop))))
 
-    (tree-menu
-     'triticale 'sown
-     (list
-      (tree-menu
-       'winter 'application (list 'forage 'incorporated 'removed))
-      (tree-menu
-       'spring 'application (list 'forage 'incorporated 'removed))))
-
-    (tree-menu
-     'oat 'sown
-     (list
-      (tree-menu
-       'winter 'application (list 'forage 'incorporated 'removed))
-      (tree-menu
-       'spring 'application (list 'forage 'incorporated 'removed))))
     
-    (tree-menu
-     'rye 'sown
-     (list
-      (tree-menu
-       'winter 'application (list 'forage 'incorporated 'removed))
-      (tree-menu
-       'spring 'application (list 'forage 'incorporated 'removed))))
-    
-    'maize
-
-    (tree-menu
-     'swede 'use
-     (list 'forage-lifted 'forage-grazed 'vegetable))
-
-    (tree-menu
-     'turnip 'use
-     (list 'forage-lifted 'forage-grazed 'vegetable))
-
-    (tree-menu
-     'rape 'use
-     (list 
-      'forage 
-      (tree-menu 
-       'oilseed 'sown
-       (list 'winter 'spring))))
-
-    'linseed
-    'pea-bean
-    'fodder-beet
-    'kale-grazed
-
-    (tree-menu
-     'brussels-sprout-cabbage 'subtype1
-     (list
-      'brussels-sprout
-      'storage-cabbage
-      'head-cabbage-pre-31-dec
-      'head-cabbage-post-31-dec
-      'collard-pre-31-dec
-      'collard-post-31-dec))
-
-    (tree-menu
-     'cauliflower-calabrese 'subtype1
-     (list
-      'cauliflower-summer-autumn
-      (tree-menu 
-       'cauliflower-winter-hardy-roscoff 'subtype2
-       (list 'seedbed 'top-dressing))
-      'calabrese))
-
-    (tree-menu
-     'lettuce-leafy-salad 'subtype1
-     (list 'lettuce-whole-head 'lettuce-baby-leaf 'wild-rocket))
-
-    (tree-menu
-     'onion-leek 'subtype1
-     (list 'bulb-onion 'salad-onion 'leek))
-
-    'beetroot
-    'carrot
-    'bulb
-    
-    (tree-menu 
-     'potato 'determinancy-group
-     (list
-      (tree-menu 
-       'one 'growing-season-length
-       (list '<60 '60-90 '90-120 '>120))
-      (tree-menu 
-       'two 'growing-season-length
-       (list '<60 '60-90 '90-120 '>120))
-      (tree-menu 
-       'three 'growing-season-length
-       (list '<60 '60-90 '90-120 '>120))
-      (tree-menu 
-       'four 'growing-season-length
-       (list '<60 '60-90 '90-120 '>120))))
-
-    (tree-menu
-     'grass 'subtype
-     (list
-      (tree-menu
-       'silage 'targetyield
-       (list
-        (tree-menu 'DM5-7 'cut (list 'one 'two 'three 'four))
-        (tree-menu 'DM7-9 'cut (list 'one 'two 'three 'four))
-        (tree-menu 'DM9-12 'cut (list 'one 'two 'three 'four))
-        (tree-menu 'DM12-15+ 'cut (list 'one 'two 'three 'four))))
-      (tree-menu
-       'grazed
-       'targetyield
-       (list 'DM4-5 'DM5-7 'DM6-8 'DM7-9 'DM9-12 'DM10-13 'DM12-15+))
-      'hay
-      (tree-menu
-       'established 'sown (list 'spring 'summer-autumn 'clover))
-      'rye)))))
+     )))
 
